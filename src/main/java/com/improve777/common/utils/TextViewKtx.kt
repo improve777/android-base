f70ee3589a1fabbf60590c1e3211af71086256c2
improve777 <improve777@gmail.com>
2021-03-18 07:42:53 +0900

    LeadingMarginSpan ext 추가

--- src/main/java/com/improve777/common/utils/TextViewKtx.kt
+++ src/main/java/com/improve777/common/utils/TextViewKtx.kt
@@ -1,2 +1,18 @@
 package com.improve777.common.utils
 
+import android.text.style.LeadingMarginSpan
+import android.widget.TextView
+import androidx.core.text.buildSpannedString
+import androidx.core.text.inSpans
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("android:text", "app:textIndent")
+fun TextView.setTextIndent(value: String, indent: Float) {
+    text = value
+    val span = LeadingMarginSpan.Standard(0, indent.toInt())
+    text = buildSpannedString {
+        inSpans(span) {
+            append(text)
+        }
+    }
+}
