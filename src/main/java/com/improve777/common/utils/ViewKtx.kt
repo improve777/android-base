08970cecd9d6499177206b99621092ea00bb7079
improve777 <improve777@gmail.com>
2021-03-21 10:32:00 +0900

    accessibleTouchTarget 메서드 추가

--- src/main/java/com/improve777/common/utils/ViewKtx.kt
+++ src/main/java/com/improve777/common/utils/ViewKtx.kt
@@ -2,6 +2,8 @@ package com.improve777.common.utils
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.graphics.Rect
+import android.view.TouchDelegate
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.inputmethod.InputMethodManager
@@ -78,4 +80,35 @@ fun View.hideKeyboard(): Boolean {
 
 inline infix fun View.click(crossinline onClick: (view: View) -> Unit) {
     setOnClickListener { onClick(it) }
+}
+
+fun View.accessibleTouchTarget() {
+    post {
+        val delegateArea = Rect()
+        getHitRect(delegateArea)
+
+        // 48 dp is the minimum requirement. We need to convert this to pixels.
+        val accessibilityMin = 48.PX
+
+        // Calculate size vertically, and adjust touch area if it's smaller then the minimum.
+        val height = delegateArea.bottom - delegateArea.top
+        if (accessibilityMin > height) {
+            // Add +1 px just in case min - height is odd and will be rounded down
+            val addition = ((accessibilityMin - height) / 2).toInt() + 1
+            delegateArea.top -= addition
+            delegateArea.bottom += addition
+        }
+
+        // Calculate size horizontally, and adjust touch area if it's smaller than the minimum.
+        val width = delegateArea.right - delegateArea.left
+        if (accessibilityMin > width) {
+            // add +1 px just in case min - width is odd and will be rounded down
+            val addition = ((accessibilityMin - width) / 2).toInt() + 1
+            delegateArea.left -= addition
+            delegateArea.right += addition
+        }
+
+        val parentView = parent as? View
+        parentView?.touchDelegate = TouchDelegate(delegateArea, this)
+    }
 }
